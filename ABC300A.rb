57244d79cc33fafd6f6a952cab88668f175f7e3e
Takahiro Nomoto <nmt.1727@gmail.com>
2024-05-05 17:26:54 +0900

    最初のコミット

---
+++ ABC300A.rb
@@ -0,0 +1,27 @@
+n = 5
+array = "TTTAA".split("")
+if n % 2 == 1
+  wins = n/2+1
+elsif n % 2 == 0
+  wins = n/2
+end
+takahashi = 0
+aoki = 0
+result = ""
+array.each do |a|
+  if a == "T"
+    takahashi += 1
+  elsif a == "A"
+    aoki += 1
+  end
+  if takahashi >= wins
+    result = "T"
+    break
+  end
+  if aoki >= wins
+    result = "A"
+    break
+  end
+end
+puts result
+
